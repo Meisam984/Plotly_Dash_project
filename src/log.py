4dcd60c80a01320328d446a78915db89aeae155e
Sam <Meysam.or.us@gmail.com>
2023-05-04 11:42:30 +0330

    Added the creation of .logs directory to log.py

--- src/log.py
+++ src/log.py
@@ -1,8 +1,11 @@
 import json
+import os
 import sys
 import logging
 import logging.config
 from datetime import datetime
+
+os.makedirs('.logs')
 
 # Set the logger config, using dictConfig and loading the log_dict_config.json
 with open(file='log_dict_config.json') as f:
